57c04d64d591d0b174d35d626d221acf7375bdc8
tsubamon55 <124486733+tsubamon55@users.noreply.github.com>
2023-10-24 00:44:34 +0900

    configの修正

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
+import os
 import configparser
 
-config = configparser.ConfigParser()
+config = configparser.RawConfigParser(os.environ)
 config.read('config.ini')
-access_token = config['line']['access_token']
-secret = config['line']['secret']
+access_token = config.get('line_api', 'access_token')
+secret = config.get('line_api', 'secret')
